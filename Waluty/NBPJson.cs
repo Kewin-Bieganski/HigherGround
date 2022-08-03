bd984c826b0a0791a408f5bc6d093ac5485d80cc
Kewin Biegański <kewinbieganski@gmail.com>
2022-08-03 23:01:42 +0200

    Aktualizacja: dodanie komentarzy do projektu Waluty

--- Waluty/NBPJson.cs
+++ Waluty/NBPJson.cs
@@ -1,11 +1,33 @@
 ﻿namespace Waluty
 {
+    /// <summary>
+    /// Klasa NBPJson reprezentuje odczyt tabeli walut
+    /// </summary>
     public class NBPJson
     {
+        /// <summary>
+        /// table - typ tabeli
+        /// </summary>
         public string table { get; set; }
+
+        /// <summary>
+        /// no - numer tabeli
+        /// </summary>
         public string no { get; set; }
+
+        /// <summary>
+        /// tradingDate - data notowania
+        /// </summary>
         public string tradingDate { get; set; }
+
+        /// <summary>
+        /// effectiveDate - data publikacji
+        /// </summary>
         public string effectiveDate { get; set; }
+
+        /// <summary>
+        /// rate - lista kursów poszczególnych walut w tabeli
+        /// </summary>
         public Rate[] rates { get; set; }
     }
 }
